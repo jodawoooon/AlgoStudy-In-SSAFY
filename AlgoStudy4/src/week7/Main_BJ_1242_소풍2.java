824fe198d9d550c2bdbe1681b8e43c0f16d108cf
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-03-31 00:22:03 +0900

    2021-03-31

--- AlgoStudy4/src/week7/Main_BJ_1242_소풍2.java
+++ AlgoStudy4/src/week7/Main_BJ_1242_소풍2.java
@@ -22,31 +22,24 @@ public class Main_BJ_1242_소풍2 {
 		M = Integer.parseInt(st.nextToken()); 
 		//동호는 M번 학생이다. 동호가 몇번으로 퇴장당하는지 구해라.
 		
-		Queue<Integer> queue = new LinkedList<>();
-		for (int i = 1; i <= N; i++) {
-			queue.add(i);
-		}
 		
 		int tgt = 0;
 		
-		while(!queue.isEmpty()) {
-			int idx = 0;
-			for (int i = 1; i < K; i++) {
-				//1번부터 K-1번까지 센다.
-				idx = queue.poll();
-				queue.add(idx); //원형구조
-			}
-			
-			//K번을 세면 아예 퇴장
-			idx = queue.poll();
-			//퇴장당한 cnt 증가
-			cnt++;
-			
-			//방금퇴장당한애가 동호이면 return;
-			if(idx==M) {
-				System.out.println(cnt);
-				return;
-			}
+		//큐 쓰니까 메모리 초과ㅏ
+		// 규칙 찾기
+		
+		// 5 2 3
+		// 1 2 3 4 5
+		// 3 4 5 1
+		// 5 1 3
+		// 3 5
+		// 3
+		//k를 외칠사람을 맨 앞으로 옮기기
+		
+		
+		//ans = 5
+		
+		for (int i = N; i < args.length; i++) {
 			
 		}
 		
