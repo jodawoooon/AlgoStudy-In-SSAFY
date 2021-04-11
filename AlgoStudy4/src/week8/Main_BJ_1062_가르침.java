2dd8bc130b72237882fe86271d21e6d0fcffb101
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-04-11 12:13:00 +0900

    2021-04-11

--- AlgoStudy4/src/week8/Main_BJ_1062_가르침.java
+++ AlgoStudy4/src/week8/Main_BJ_1062_가르침.java
@@ -7,6 +7,21 @@ import java.util.StringTokenizer;
 
 public class Main_BJ_1062_가르침 {
 	
+	/*
+	 * DFS (comb)
+	 * 
+	 * 조합으로 K개의 글자를 선택해서 문제를 풀었습니다.
+	 * K개의 글자 중 a,n,t,i,c은 항상 포함 되야 하므로
+	 * K가 5미만이면 ,
+	 * 답은 항상 0이 됩니다. 그래서 이를 먼저 확인하고 return했고
+	 * K가 5이상이라면,
+	 * 먼저 읽을 수 있는 K개의 글자 (check)에 a,n,t,i,c을 포함시킵니다.
+	 * 그리고 조합(dfs)으로 k-5개의 글자를 선택하고
+	 * 해당 글자로 단어들을 읽으며 단어의 첫 글자부터 끝까지 읽을 수 있다면 flag를 true를 두어 res++했습니다. 그리고 res의 max값인 ans를 출력합니다.
+	 * 
+	 * 메모리 13372	| 시간 348
+	 */
+	
 	static int N, K, ans;
 	static ArrayList<String> words;
 	static boolean[] check;
@@ -50,19 +65,19 @@ public class Main_BJ_1062_가르침 {
 		
 		//26개중에 이제 골라야된다
 		ans = Integer.MIN_VALUE;
-		dfs(0, 0);
+		comb(0, 0);
 		
 		System.out.println(ans);
 		
 		
 	}
-	private static void dfs(int idx, int cnt) {
+	private static void comb(int idx, int cnt) {
 		
 		if( cnt == K-5) {
-			//단어 다 골랐으면
+			//글자를 다 골랐으면
 			
 			int res = 0;
-			
+			//단어를 읽는다
 			for (int i = 0; i < N; i++) {
 				String word = words.get(i);
 				boolean flag = true;
@@ -71,8 +86,11 @@ public class Main_BJ_1062_가르침 {
 					
 					if(!check[c-'a']) {
 						flag = false;
+						//하나라도 못읽으면 flag는 false
+						break;
 					}
 				}	
+				//flag가 true이면 res++
 				if(flag) res++;
 
 			}
@@ -88,7 +106,7 @@ public class Main_BJ_1062_가르침 {
 		for (int i = idx; i < 26; i++) {
 			if(!check[i]) {
 				check[i] = true;
-				dfs(i+1, cnt+1);
+				comb(i+1, cnt+1);
 				check[i] = false;
 			}
 		}
