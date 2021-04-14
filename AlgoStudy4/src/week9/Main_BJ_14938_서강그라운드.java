49b77ea8944a706de8150ecc182f36cb5ef7d407
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-04-14 15:36:58 +0900

    2021-04-14

--- AlgoStudy4/src/week9/Main_BJ_14938_서강그라운드.java
+++ AlgoStudy4/src/week9/Main_BJ_14938_서강그라운드.java
@@ -21,13 +21,14 @@ public class Main_BJ_14938_서강그라운드 {
 		m = Integer.parseInt(st.nextToken()); //수색범위
 		r = Integer.parseInt(st.nextToken()); //길의 개수
 		
-		
+		int ans = Integer.MIN_VALUE;
+
 		adjList = new ArrayList[n+1]; //인접리스트
 		for (int i = 1; i <= n; i++) {
 			adjList[i] = new ArrayList<>();
 		}
 		
-		//다익스트라
+	
 		arr = new int[n+1];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= n; i++) {
@@ -43,10 +44,8 @@ public class Main_BJ_14938_서강그라운드 {
 			adjList[x].add(new Node(y,l)); //양방향연결
 			adjList[y].add(new Node(x,l));
 		}
-		
-		
-		
-		//PQ 선언
+
+	
 		PriorityQueue<Node> pq = new PriorityQueue<>(new Comparator<Node>() {
 
 			@Override
@@ -55,20 +54,24 @@ public class Main_BJ_14938_서강그라운드 {
 			}
 			
 		});
+		//PQ 선언
+
 		
 		int[] weight = new int[n+1];
-		
-		int ans = Integer.MIN_VALUE;
-		for (int i = 1; i <= n; i++) {
-			//가중치를 저장할 배열
+		//가중치를 저장할 배열
+
+		for (int i = 1; i <= n; i++) { //예은이의 착륙지점
 			
 			for (int k = 1; k <= n; k++) {
 				weight[k] = INF;
 			}
+			//가중치 배열 초기화
+
+
 			
 			weight[i] = 0;
 			pq.add(new Node(i,0));
-			
+			//시작점 설정
 
 			while(!pq.isEmpty()) {
 				Node n = pq.poll();
@@ -83,8 +86,7 @@ public class Main_BJ_14938_서강그라운드 {
 					int to = node.v;
 					int tWeight = node.weight + fWeight;
 					
-
-					if(tWeight>m) continue;
+					if(tWeight>m) continue; //수색범위를 넘어가면 continue
 					
 					weight[to] = Math.min(tWeight, weight[to]);
 					
@@ -95,24 +97,20 @@ public class Main_BJ_14938_서강그라운드 {
 			int tmp = 0;
 			for (int k = 1; k <= n; k++) {
 				//System.out.println(weight[k]);
-				
-				if(weight[k]!=INF) {
-					tmp += arr[k];
+				if(weight[k]!=INF) { //이동가능한 정점이면
+					tmp += arr[k]; //아이템수 누적
 				}
-				
 			}
 			
-			ans = Math.max(tmp, ans);
+			ans = Math.max(tmp, ans); // max 아이템수
 			
 		}
-		
-		
-		
+
 		System.out.println(ans);
-		
-		
+
 	}
 	
+
 	static class Node{
 		int v, weight;
 
