56711e6957b3932a36ab2c7186609daad5b4e0b5
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-04-24 23:12:50 +0900

    2021-04-24

--- AlgoStudy4/src/week10/Main_BJ_11967_불켜기.java
+++ AlgoStudy4/src/week10/Main_BJ_11967_불켜기.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
 
 
 public class Main_BJ_11967_불켜기 {
-	
+
 	static class Node{
 		int x, y;
 
@@ -37,13 +37,11 @@ public class Main_BJ_11967_불켜기 {
 		ans = 1;
 		map = new int[N+1][N+1];
 		list = new ArrayList[N+1][N+1];
-		
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <=N; j++) {
 				list[i][j] = new ArrayList<>();
 			}
 		}
-		
 		
 		
 		for (int m = 0; m < M; m++) {
@@ -53,8 +51,7 @@ public class Main_BJ_11967_불켜기 {
 			int y = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
-			
+
 			list[x][y].add(new Node(a,b));
 			
 		}
@@ -63,21 +60,23 @@ public class Main_BJ_11967_불켜기 {
 		//각 방에서는 상하좌우에 인접한 방으로 움직일 수 있다. 
 		
 		
-		ans = bfs(1,1);
-
+		bfs(1,1);
+//		for (int[] ia : map) {
+//			System.out.println(Arrays.toString(ia));
+//		}
 		
 		System.out.println(ans);
 	}
 
-	private static int bfs(int i, int j) {
+	private static void bfs(int i, int j) {
 		Queue<Node> queue = new LinkedList<>();
 		boolean visited[][] = new boolean[N+1][N+1];
 		
+		map[i][j] = 1; //처음 위치 불 밝히기
 		visited[i][j] = true;
-		map[i][j] = 1;
 		
 		queue.add(new Node(i,j));
-		int cnt = 1;
+		
 		
 		while(!queue.isEmpty()) {
 			Node n = queue.poll();
@@ -85,45 +84,32 @@ public class Main_BJ_11967_불켜기 {
 			int x = n.x;
 			int y = n.y;
 			
-			int size = 0;
-			if(list[x][y]!=null) {
-				size = list[x][y].size();
-			}
-			
-			
+			int size = list[x][y].size();
 			
 			//list에서 정보를 가져와서, 킬 수 있는 방의 불을 킨다
-			for(int k = 0 ;k <size ; k++) {
+			for (int k = 0; k < size ; k++) {
 				Node node = list[x][y].get(k);
 				int nx = node.x;
 				int ny = node.y;
 				
-				if(visited[nx][ny]) continue;
-				if(map[nx][ny]==1) continue;
-				//이미 불 켰으면 continue
+				if(map[nx][ny]==1) continue; //불이 이미 켜져있으면 또 킬 필요 X
 				
-				map[nx][ny] = 1;
-				//불을 킨다.
-				++cnt;
-				//불 켜진 방 갯수 증가
-				
+				//불이 꺼져있으면 킨다
+				map[nx][ny] = 1; //불을 킨다 
+				ans++; //불켜진 방 개수 ++
 				
 				//이동가능한 방인지 확인한다
+				
 				for (int d = 0; d < 4; d++) {
 					int nnx = nx + dx[d];
 					int nny = ny + dy[d];
 					
 					if(nnx<=0||nny<=0||nnx>N||nny>N) continue;
-					if(!visited[nnx][nny]) continue;
-					// 베시는 불이 켜져있는 방으로만 들어갈 수 있다. => 
-					//불을 킨 곳 map[nx][ny]의 인접한 방 중 방문했던 방이 있다면
-					//방금 내가 불 킨 map[nx][ny]에 방문할 수 있게된다.
-					
-					visited[nx][ny] = true;
-						//방문가능한곳이면 q에 넣는다
-					queue.add(node);
-					break;
-					
+					if(visited[nnx][nny]) {
+						//내가 불 킨 곳 주변에 방문가능하면 => (nx,ny)에 방문 가능하다
+						queue.add(new Node(nnx,nny)); //방문가능한곳이면 q에 넣는다
+						break;
+					}
 					
 				}
 				
@@ -134,21 +120,17 @@ public class Main_BJ_11967_불켜기 {
 			for (int d = 0; d < 4; d++) {
 				int nx = x + dx[d];
 				int ny = y + dy[d];
-				
-				
 				if(nx<=0||ny<=0||nx>N||ny>N) continue;
+				
+				
+				
 				if(visited[nx][ny]) continue;
 				if(map[nx][ny]==0) continue;
 				
-				//System.out.println(x+" "+y+" / "+nx+" "+ny);
-				
-				//불이켜져있으면 이동
-				visited[nx][ny] = true;
+				//불 켜져 있는데 내가 간 적 없으면
+				visited[nx][ny] = true; //이동
 				queue.add(new Node(nx,ny));
 			}
 		}
-		
-		return cnt;
-		
 	}
 }
