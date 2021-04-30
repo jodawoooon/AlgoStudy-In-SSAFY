5825b8689cddd65a4c131bb9f093640535a1f395
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-05-01 00:17:44 +0900

    2021-05-01

--- AlgoStudy4/src/week11/Main_BJ_6087_레이저통신.java
+++ AlgoStudy4/src/week11/Main_BJ_6087_레이저통신.java
@@ -14,7 +14,7 @@ public class Main_BJ_6087_레이저통신 {
 	// C에서만 발사할 수 있고, 빈 칸에 거울('/', '\')을 설치해서 방향을 90도 회전시킬 수 있다. 
 	
 	// '/' ; 왼쪽반사, '\' ; 오른쪽반사
-	static int W,H, mirror[][];
+	static int W,H, point[][], ans;
 	static char map[][];
 	static int dx[] = {-1,0,1,0}; //90도씩 우향우 -> 순서 : 상 우 하 좌 
 	static int dy[] = {0,1,0,-1};
@@ -43,49 +43,78 @@ public class Main_BJ_6087_레이저통신 {
 		W = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
 		
+		ans = Integer.MAX_VALUE;
 		map = new char[H][W];
-		mirror = new int[2][2];
+		point = new int[2][2];
 		int idx = 0;
 		for (int h = 0; h < H; h++) {
 			String str = br.readLine();
 			for (int w = 0; w < W; w++) {
 				map[h][w] = str.charAt(w);
 				if(map[h][w]=='C') {
-					mirror[idx][0] = h;
-					mirror[idx++][1] = w;
+					point[idx][0] = h;
+					point[idx++][1] = w;
 				}
 			}
 		}
 		
 		bfs();
 		
+		
+		System.out.println(ans);
+		
 	}
 	private static void bfs() {
 		Queue<Node> queue = new LinkedList<>();
+		boolean visited[][][] = new boolean[H][W][4];
 		
 		//4방 출발
-		queue.add(new Node(mirror[0][0], mirror[0][1],0,0));
-		queue.add(new Node(mirror[0][0], mirror[0][1],1,0));
-		queue.add(new Node(mirror[0][0], mirror[0][1],2,0));
-		queue.add(new Node(mirror[0][0], mirror[0][1],3,0));
+		for (int d = 0; d < 4; d++) {
+			queue.add(new Node(point[0][0], point[0][1],d,0));
+			visited[point[0][0]][point[0][1]][d]=true;
+		}
 		
 		while(true) {
 			Node n = queue.poll();
 			int x = n.x;
 			int y = n.y;
+			int d = n.dir;
+			//System.out.println("===============");
+			//System.out.println(x+" "+y+" "+d+" "+n.cnt);
 			
-			for (int d = 0; d < 4; d++) {
-				int nx = x + dx[d];
-				int ny = y + dy[d];
-				
-				if(!isIn(nx,ny)) continue;
+			if(x==point[1][0]&&y==point[1][1]) { //최종위치 도착하면
+				//System.out.println(n.cnt);
+				//ans = Math.min(ans, n.cnt); //거울몇개썼는지 갱신
+				break;
 			}
+			
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			
+			if(!isIn(nx,ny)) continue;
+			
+			if(visited[nx][ny][d]) continue;
+			if(map[nx][ny]=='*') continue;
+			visited[nx][ny][d] = true;
+			
+			//System.out.println(nx+" "+ny+" "+d+" "+n.cnt);
+			//System.out.println(map[nx][ny]);
+//			System.out.println("===============");
+			
+			
+			if(map[nx][ny]=='C') ans = Math.min(ans, n.cnt);
+			queue.add(new Node(nx,ny,d,n.cnt)); //그냥 직진
+		
+			queue.add(new Node(nx,ny,(d+1)%4, n.cnt+1)); //오른쪽 회전 거울 세우기
+			queue.add(new Node(nx,ny,(d+3)%4, n.cnt+1)); //왼쪽회전
+				
 		}
+		
 		
 	}
 	
 	private static boolean isIn(int nx, int ny) {
-		if(nx<0||ny<0||nx>H||ny>W) return false; //맵 range 체크
+		if(nx<0||ny<0||nx>=H||ny>=W) return false; //맵 range 체크
 		return true;
 	}
 }
