43ecb5f0d2d075ceb19c54b60f602e08796982b4
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-04-30 23:10:06 +0900

    2021-04-30

--- AlgoStudy4/src/week11/Main_BJ_3709_레이저빔은어디로.java
+++ AlgoStudy4/src/week11/Main_BJ_3709_레이저빔은어디로.java
@@ -17,7 +17,7 @@ public class Main_BJ_3709_레이저빔은어디로 {
 	}
 	static int N, R, map[][], dir;
 	static Node ans;
-	static int dx[] = {-1,0,1,0}; //상 우 하 좌 => 우향우
+	static int dx[] = {-1,0,1,0}; //90도씩 우향우 -> 순서 : 상 우 하 좌 
 	static int dy[] = {0,1,0,-1};
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,7 +28,7 @@ public class Main_BJ_3709_레이저빔은어디로 {
 			N = Integer.parseInt(st.nextToken()); //보드의 크기
 			R = Integer.parseInt(st.nextToken()); //우향우 거울의 개수
 			
-			map = new int[N+2][N+2];
+			map = new int[N+2][N+2]; //0부터 N+1까지의 맵을 가진다
 			
 			for (int r = 0; r < R; r++) {
 				//우향우 거울이 배치된 좌표 x y
@@ -37,7 +37,7 @@ public class Main_BJ_3709_레이저빔은어디로 {
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
 				
-				map[x][y] = -1; //우향우거울 맵핑
+				map[x][y] = -1; //우향우거울 위치 맵핑
 			}
 			
 			//레이저의 좌표
@@ -45,10 +45,10 @@ public class Main_BJ_3709_레이저빔은어디로 {
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
 			
-			map[x][y] = 1; //레이저 맵핑
+			map[x][y] = 1; //레이저위치 맵핑
 			
 			ans = new Node(0,0);
-			dir = checkDir(x,y);
+			dir = checkDir(x,y); //레이저가 어느 방향을 향하고 있는지 찾는다
 			
 			dfs(x,y,dir);
 			
@@ -62,25 +62,25 @@ public class Main_BJ_3709_레이저빔은어디로 {
 	
 	private static void dfs(int x, int y, int d) {
 		
-		int nx = x + dx[d];
+		int nx = x + dx[d]; //위치
 		int ny = y + dy[d];
 		
-		if(!isIn(nx,ny)) {
+		if(!isIn(nx,ny)) { //맵의 공간을 넘어서면, 결과 (x,y)를 갱신
 			ans.x = nx;
 			ans.y = ny;
 			return;
 		}
 		
-		if(map[nx][ny]==-1) {
-			d = (d+1)%4;
+		if(map[nx][ny]==-1) { //우향우 거울을 만나면 
+			d = (d+1)%4; //방향을 90도 꺾는다
 			dfs(nx,ny,d);
-		}else if(map[nx][ny]==0) {
+		}else if(map[nx][ny]==0) { //거울안만나면 그냥 방향 그대로 dfs
 			dfs(nx,ny,d);
 		}
 
 	}
 	private static boolean isIn(int nx, int ny) {
-		if(nx<1||ny<1||nx>N||ny>N) return false;
+		if(nx<1||ny<1||nx>N||ny>N) return false; //맵 range 체크
 		return true;
 	}
 
