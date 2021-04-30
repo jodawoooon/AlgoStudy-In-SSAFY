9fd0d2856e16f278d7c89f9679b0b300d8025df6
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-04-30 23:18:26 +0900

    2021-04-30

--- AlgoStudy4/src/week11/Main_BJ_6087_레이저통신.java
+++ AlgoStudy4/src/week11/Main_BJ_6087_레이저통신.java
@@ -2,9 +2,38 @@ package week11;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main_BJ_6087_레이저통신 {
+	
+	// W×H 크기의 지도
+	// 지도의 각 칸은 빈 칸이거나 벽이며, 두 칸은 'C'로 표시되어 있는 칸이다.
+	// 'C'로 표시되어 있는 두 칸을 레이저로 통신하기 위해서 설치해야 하는 거울 개수의 최솟값
+	// C에서만 발사할 수 있고, 빈 칸에 거울('/', '\')을 설치해서 방향을 90도 회전시킬 수 있다. 
+	
+	// '/' ; 왼쪽반사, '\' ; 오른쪽반사
+	static int W,H;
+	static char map[][];
+	static int dx[] = {-1,0,1,0}; //90도씩 우향우 -> 순서 : 상 우 하 좌 
+	static int dy[] = {0,1,0,-1};
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		W = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		
+		map = new char[H][W];
+		
+		for (int h = 0; h < H; h++) {
+			String str = br.readLine();
+			for (int w = 0; w < W; w++) {
+				map[h][w] = str.charAt(w);
+			}
+		}
+		
+		
+		
 	}
 }
