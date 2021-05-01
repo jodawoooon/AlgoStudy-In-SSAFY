24ea045d07006ce9eb1649ec6a7e65fe096c1fb8
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-05-02 01:58:55 +0900

    2021-05-02

--- AlgoStudy4/src/week11/Main_BJ_3709_레이저빔은어디로.java
+++ AlgoStudy4/src/week11/Main_BJ_3709_레이저빔은어디로.java
@@ -5,6 +5,21 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_BJ_3709_레이저빔은어디로 {
+	
+/*	dfs로 풀었습니다
+	- 우향우 거울에 레이저가 반사되므로 
+	방향배열의 순서를 90도씩 오른쪽으로 회전하도록 상,우,하,좌로 설정하였습니다.
+	그리고 처음에 레이저가 어느 방향을 향하는지 찾고, 
+	그 위치와 dir를 가지고 dfs를 돌렸습니다.
+
+	그리고 dfs안에서 한칸씩 이동하면서 우향우거울을 만나면 dir=(dir+1)%4해서 90도씩 꺾어주었습니다.
+	그리고 맵을 탈출하면 해당 위치를 return합니다.
+
+
+	- 결과
+	12012	88
+	
+	*/
 	static class Node{
 		int x,y;
 
