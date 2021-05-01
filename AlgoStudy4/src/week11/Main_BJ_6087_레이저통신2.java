2b08904193fb104e3031ee3949740ab8976862bd
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-05-02 00:14:35 +0900

    2021-05-02

--- AlgoStudy4/src/week11/Main_BJ_6087_레이저통신2.java
+++ AlgoStudy4/src/week11/Main_BJ_6087_레이저통신2.java
@@ -2,11 +2,12 @@ package week11;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main_BJ_6087_레이저통신 {
+public class Main_BJ_6087_레이저통신2 {
 	
 	static int W,H, point[][], ans;
 	static char map[][];
@@ -18,18 +19,6 @@ public class Main_BJ_6087_레이저통신 {
 		int x,y;
 		int dir;
 		int cnt;
-
-
-		
-		public Node(int x, int y, int cnt) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.cnt = cnt;
-		}
-
-
-
 		public Node(int x, int y, int dir, int cnt) {
 			super();
 			this.x = x;
@@ -50,68 +39,67 @@ public class Main_BJ_6087_레이저통신 {
 		ans = Integer.MAX_VALUE;
 		map = new char[H][W];
 		point = new int[2][2];
+
 		int idx = 0;
 		for (int h = 0; h < H; h++) {
 			String str = br.readLine();
 			for (int w = 0; w < W; w++) {
 				map[h][w] = str.charAt(w);
-				if(map[h][w]=='C') {
+				if(map[h][w]=='C') { //레이저의 위치 저장
 					point[idx][0] = h;
-					point[idx++][1] = w;
+					point[idx][1] = w;
+					idx++;
 				}
+				
 			}
+			
 		}
 		
 		bfs();
 		
 		
-		System.out.println(ans-1);
+		System.out.println(ans);
 		
 	}
+	
 	private static void bfs() {
 		Queue<Node> queue = new LinkedList<>();
-		//boolean visited[][][] = new boolean[H][W][4];
-		boolean visited[][] = new boolean[H][W];
+		int visited[][] = new int[H][W];
 		
-
-		
-		queue.add(new Node(point[0][0], point[0][1],0));
-		visited[point[0][0]][point[0][1]] = true;
+		for (int d = 0; d < 4; d++) { //4방탐색 시작
+			queue.add(new Node(point[0][0], point[0][1], d, 0));
+			
+		}
+		visited[point[0][0]][point[0][1]]=1; //visit체크
+	
 		
 		while(!queue.isEmpty()) {
 			Node n = queue.poll();
 			int x = n.x;
 			int y = n.y;
+			int d = n.dir;
+			int nc = n.cnt;
 			
 			if(x==point[1][0]&&y==point[1][1]) { //최종위치 도착하면
 				//System.out.println(n.cnt);
 				ans = Math.min(ans, n.cnt); //거울몇개썼는지 갱신
-				break;
+				continue;
 			}
 			
-			for (int d = 0; d < 4; d++) {
-				int nx = x;
-				int ny = y;
-				
-				while(true) {
-					//벽을 만나기 전까지 or 맵을 벗어나기 전까지
-					//if(visited[nx][ny]) break;
-					
-					if(!isIn(nx,ny)) break;
-					if(map[nx][ny]=='*') break;
-					
-					if(!visited[nx][ny]) {
-						visited[nx][ny]=true;
-						queue.add(new Node(nx,ny,n.cnt+1));
-					}
+			
+			
+			int nx = x + dx[d];
+			int ny = y + dy[d];
 
-					nx+=dx[d];
-					ny+=dy[d];
-				}
-
+			if(!isIn(nx,ny)) continue;
+			if(map[nx][ny]=='*') continue;
+			if(visited[nx][ny]==0||visited[nx][ny]>=nc){ //방문 안했으면 간다 //방문 했으나 더 적은 cost로 갱신 가능하면 간다
+				visited[nx][ny] = nc;
+				queue.add(new Node(nx,ny,d,nc)); //직진
+				queue.add(new Node(nx,ny,(d+1)%4,nc+1)); // 오른쪽90도회전
+				queue.add(new Node(nx,ny,(d+3)%4,nc+1)); // 왼쪽90도 회전
 			}
-
-				
+			
 		}
 		
 		
