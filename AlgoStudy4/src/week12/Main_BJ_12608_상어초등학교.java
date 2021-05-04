b725ba4b21e13105a0482c6c9f27449820d47df5
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-05-04 20:58:52 +0900

    2021-05-04

--- AlgoStudy4/src/week12/Main_BJ_12608_상어초등학교.java
+++ AlgoStudy4/src/week12/Main_BJ_12608_상어초등학교.java
@@ -1,5 +1,24 @@
 package week12;
 
-public class Main_BJ_12608_상어초등학교 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main_BJ_12608_상어초등학교 {
+	
+	static int N, map[][];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		map = new int[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
 }
