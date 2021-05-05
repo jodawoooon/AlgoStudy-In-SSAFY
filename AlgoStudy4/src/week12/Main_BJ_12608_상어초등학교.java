e879e4aa72efd6a46ee0acd75cbcdff5631a8c7b
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-05-05 23:19:51 +0900

    2021-05-05

--- AlgoStudy4/src/week12/Main_BJ_12608_상어초등학교.java
+++ AlgoStudy4/src/week12/Main_BJ_12608_상어초등학교.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main_BJ_12608_상어초등학교 {
-	
+
 	static int N, map[][];
 	static ArrayList<Integer>[] list;
+	static int dx[]= {-1,1,0,0};
+	static int dy[]= {0,0,-1,1};
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -30,7 +32,32 @@ public class Main_BJ_12608_상어초등학교 {
 		}
 		//list 셋팅
 		
+		/*	
+		1. 비어있는 칸 중에서 좋아하는 학생이 인접한 칸에 가장 많은 칸으로 자리를 정한다.
+		2. 1을 만족하는 칸이 여러 개이면, 인접한 칸 중에서 비어있는 칸이 가장 많은 칸으로 자리를 정한다.
+		3. 2를 만족하는 칸도 여러 개인 경우에는 행의 번호가 가장 작은 칸으로, 그러한 칸도 여러 개이면 열의 번호가 가장 작은 칸으로 자리를 정한다.
+		*/
 		
+		for (int idx = 1; idx <= N*N; idx++) {
+			int[]
+			
+			for (int x = 0; x < N; x++) {
+				for (int y = 0; y < N; y++) {
+					if(map[x][y]!=0) continue;
+					
+					for (int d = 0; d < 4; d++) {
+						int nx = x + dx[d];
+						int ny = y + dy[d];
+						
+						if(nx<0||ny<0||nx>=N||ny>=N) continue;
+						
+						
+					}
+				}
+			}
+			
+			
+		}
 		
 		
 		
