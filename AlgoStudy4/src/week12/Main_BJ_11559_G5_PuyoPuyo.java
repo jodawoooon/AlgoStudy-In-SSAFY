ee5848ed963cb5e2dc8fcdc2d228cc9454f38837
jodawoooon <38212743+jodawoooon@users.noreply.github.com>
2021-05-06 21:32:56 +0900

    2021-05-06

--- AlgoStudy4/src/week12/Main_BJ_11559_G5_PuyoPuyo.java
+++ AlgoStudy4/src/week12/Main_BJ_11559_G5_PuyoPuyo.java
@@ -1,5 +1,11 @@
 package week12;
 
-public class Main_BJ_11559_G5_PuyoPuyo {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Main_BJ_11559_G5_PuyoPuyo {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	}
 }
