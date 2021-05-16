2756e28216a9087c0b558a7b4bf6d01037032ea7
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-05-16 20:15:06 +0900

    2021-05-16

---
+++ AlgoStudy4/src/week13/Main_BJ_2878_캔디캔디.java
@@ -0,0 +1,17 @@
+package week13;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_BJ_2878_캔디캔디 {
+	
+	static int M,N;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+	}
+}
