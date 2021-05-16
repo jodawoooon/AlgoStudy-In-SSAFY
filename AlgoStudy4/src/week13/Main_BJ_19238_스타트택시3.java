739041a44bb30db606d6e29e1860da83cba95d32
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-05-16 20:13:28 +0900

    2021-05-16

--- AlgoStudy4/src/week13/Main_BJ_19238_스타트택시3.java
+++ AlgoStudy4/src/week13/Main_BJ_19238_스타트택시3.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
