afbb3ee856f40f9c9f431af1af1d9b85186c6b1e
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-05-16 20:56:33 +0900

    2021-05-16

--- AlgoStudy4/src/week13/Main_BJ_2878_캔디캔디.java
+++ AlgoStudy4/src/week13/Main_BJ_2878_캔디캔디.java
@@ -7,6 +7,29 @@ import java.util.StringTokenizer;
 
 public class Main_BJ_2878_캔디캔디 {
 	
+/*	
+	먼저 부족한 사탕의 개수를 구합니다. (sum-M)
+	그리고 못 주는 사탕을 공평하게 배분 (부족한 사탕 / 나누어 줄 친구 수 => sum / N ) 하여 사탕을 나눕니다.
+
+	가장 사탕을 덜 받고 싶어하는 친구부터 배분을 시작합니다.
+	이 때 사탕을 받고 싶어하는 갯수가, 못 주는 사탕의 개수보다 작다면
+	굳이 못 주는 사탕의 수를 크게 할 필요가 없으므로 min값으로 사탕을 분배하고 분노수치를 구한다.
+
+
+
+	- 어려웠던점
+	많이 요구한 사람에게는 많이주고, 조금 요구한사람한테는 조금 주기위해
+	가장 많이받고싶은 사람에게 먼저 분배하는 식으로 구현했으나 실패했습니다.
+
+	해당 로직을 구현하기가 어려워서 풀이를 참고했고
+	사탕을 나누지 않고, 부족한 사탕의 개수를 적절히 분배해도 된다는 점을 깨달았습니다.
+
+
+
+	- 결과
+	21824	256
+	
+	*/
 	static int M,N,want[];
 	static long sum,ans;
 	public static void main(String[] args) throws Exception {
@@ -28,14 +51,16 @@ public class Main_BJ_2878_캔디캔디 {
 		
 		//사탕을 적절히 나누어 주어 친구들의 분노의 합을 최소화
 		
-		sum -= M;
-		Arrays.sort(want);
+		sum -= M; //못주는 사탕의 수
+		Arrays.sort(want); //오름차순 정렬
 		
 		
 		for (int i = 0; i < want.length; i++) {
 			long w = Math.min(want[i], sum / N--);
-			ans += w*w;
-			sum -= w;
+			//갖고싶은 사탕의 수가 분배값보다 작다면? 이 값으로 바꾼다
+			
+			ans += w*w; //분노수치 갱신
+			sum -= w; //사탕 수 갱신
 		}
 		
 		System.out.println(ans%((long)Math.pow(2, 64)));
