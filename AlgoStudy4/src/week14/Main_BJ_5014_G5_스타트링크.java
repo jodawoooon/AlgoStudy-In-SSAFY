609e894775eded8749c18ad6e161d6e9b0675823
조다운 <38212743+jodawoooon@users.noreply.github.com>
2021-05-27 22:30:59 +0900

    2021-05-27 스타트링크

---
+++ AlgoStudy4/src/week14/Main_BJ_5014_G5_스타트링크.java
@@ -0,0 +1,14 @@
+package week14;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_BJ_5014_G5_스타트링크 {
+
+	//1층~F층
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
